56e5d5fadd95caff8f10e666ffdbe86b158b8ddb
Andrea Kim <andreaforanimals@gmail.com>
2016-07-05 22:53:32 -0500

    working on it

--- starter-code/UserInfoTabs/app/src/main/java/ly/generalassemb/drewmahrt/userinfotabs/MainActivity.java
+++ starter-code/UserInfoTabs/app/src/main/java/ly/generalassemb/drewmahrt/userinfotabs/MainActivity.java
@@ -1,6 +1,8 @@
 package ly.generalassemb.drewmahrt.userinfotabs;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +12,9 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +23,50 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Picture"));
+        tabLayout.addTab(tabLayout.newTab().setText("Photo"));
         tabLayout.addTab(tabLayout.newTab().setText("About Me"));
         tabLayout.addTab(tabLayout.newTab().setText("Contact"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+
+        PhotoFragment newFragment = new PhotoFragment();
+        Bundle args = new Bundle();
+        //  args.putInt(PhotoFragment.newInstance(), tabLayout);
+        newFragment.setArguments(args);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        // ft.replace(R.id.tab_layout,newFragment);
+        // ft.addToBackStack(null);
+        // ft.commit();
+
+        PhotoFragment photo = new PhotoFragment();
+        Bundle photoBundle = new Bundle();
+        String photoName = getResources().getString(R.string.photo_text);
+        photoBundle.putString("PHOTO", photoName);
+        photo.setArguments(photoBundle);
+        ft.add(R.id.tab_layout,photo);
+        ft.commit();
+
+        FragmentTransaction ftx = fm.beginTransaction();
+        InfoFragment info = new InfoFragment();
+        Bundle infoBundle = new Bundle();
+        String infoName = getResources().getString(R.string.info_text);
+        infoBundle.putString("INFO", infoName);
+        info.setArguments(photoBundle);
+        ftx.add(R.id.tab_layout,info);
+        ftx.commit();
+
+        FragmentTransaction ftz = fm.beginTransaction();
+        ContactFragment contact = new ContactFragment();
+        Bundle contactBundle = new Bundle();
+        String contactName = getResources().getString(R.string.contact);
+        contactBundle.putString("INFO", contactName);
+        contact.setArguments(contactBundle);
+        ftz.add(R.id.tab_layout,contact);
+        ftz.commit();
+
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         final PagerAdapter adapter = new PagerAdapter
@@ -32,6 +77,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
+//                PhotoFragment photoFrag = (PhotoFragment);
+//                InfoFragment infoFrag = (InfoFragment);
+//                ContactFragment contactFrag = (ContactFragment);
+//                getSupportFragmentManager().findFragmentById(R.id.pager);
             }
 
             @Override
@@ -44,5 +93,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-    }
+
+    }   //end braces for onCreate method is here :)
 }
